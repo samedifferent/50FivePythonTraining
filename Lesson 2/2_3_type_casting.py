49b47eaae309e5c3f6549e6eeafe09d002d86218
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2023-03-07 12:13:00 -0500

    Typo

--- Lesson 2/2_3_type_casting.py
+++ Lesson 2/2_3_type_casting.py
@@ -1,6 +1,7 @@
 # Let's say we have an integer variable called "profit" and we want to print it with some text.
 profit = 100
-# print("Total profit: " + profit) This gives us an error because we can only combine strings. We need to convert this integer to a string.
+# print("Total profit: " + profit) This gives us an error because we can only combine strings.
+# We need to convert this integer to a string.
 
 # To cast our integer to a string, we use the following function:
 print("Total profit: " + str(profit))
